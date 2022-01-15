a3f461e337e38863d1f0a2fffb6959bb1cd04852
Karina <puentek@oregonstate.edu>
2022-01-15 13:25:16 -0800

    2.7.4 part

---
+++ example_scripts/2_7_4ex.py
@@ -0,0 +1,26 @@
+from picarx_improved import Picarx
+import time 
+
+if __name__ == "__main__":
+    px = Picarx()
+    px.set_dir_servo_angle(0)
+    px.forward(25)
+    time.sleep(1)
+    px.stop()
+    
+#     time.sleep(1)
+#     px.self.set_motor_speed(1, 1)
+#     px.self.set_motor_speed(2, 1)
+#     px.camera_servo_pin.angle(0)
+# set_camera_servo1_angle(cam_cal_value_1)
+# set_camera_servo2_angle(cam_cal_value_2)
+# set_dir_servo_angle(dir_cal_value)
+
+# if __name__ == "__main__":
+#     try:
+#         # dir_servo_angle_calibration(0) 
+#         while 1:
+#             test()
+#     finally: 
+#         stop()
+
---
+++ example_scripts/exa.py
@@ -0,0 +1,6 @@
+import picarx_improved
+from picarx_improved import Picarx
+
+if __name__ == "__main__":
+    picar = Picarx()
+    print("I'm first")
