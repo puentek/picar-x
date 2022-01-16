d8d78c0ae72488c963c43c91ad12cc1c8670703f
Karina <puentek@oregonstate.edu>
2022-01-15 18:02:18 -0800

    left changes

--- example_scripts/parallel_left.py
+++ example_scripts/parallel_left.py
@@ -16,36 +16,40 @@ if __name__ == "__main__":
     time.sleep(2)
     px.stop()
 
+    px.forward(50)
+    time.sleep(.5)
+    px.stop()
+    time.sleep(.5)
+
     px.backward(50)
-    time.sleep(.5)
-    px.stop()
-    time.sleep(.5)
-
-    px.forward(50)
     time.sleep(.25)
     px.stop()
+    time.sleep(.1)
 
     px.set_dir_servo_angle(-35)
     time.sleep(2)
     px.stop()
 
-    px.forward(50)
+    px.backward(50)
     time.sleep(.25)
     px.stop()
+    time.sleep(.1)
 
     px.set_dir_servo_angle(-35)
     time.sleep(2)
     px.stop()
 
-    px.forward(50)
+    px.backward(50)
     time.sleep(.25)
     px.stop()
+    time.sleep(.1)
 
     px.set_dir_servo_angle(0)
     time.sleep(2)
     px.stop()
+    time.sleep(.1)
 
-    px.backward(50)
+    px.forward(50)
     time.sleep(.25)
     px.stop()
     time.sleep(.1)
