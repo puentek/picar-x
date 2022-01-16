bc654e1d3a0f30b605f2d83d0a2bf69996613d00
Karina <puentek@oregonstate.edu>
2022-01-15 18:26:42 -0800

    right changes

--- example_scripts/parallel_right.py
+++ example_scripts/parallel_right.py
@@ -12,42 +12,30 @@ if __name__ == "__main__":
     px.stop()
     time.sleep(.1)
 
-    px.set_dir_servo_angle(30)
+    px.set_dir_servo_angle(-30)
     time.sleep(2)
     px.stop()
 
     px.forward(50)
-    time.sleep(.5)
+    time.sleep(.75)
     px.stop()
-    time.sleep(.5)
+    time.sleep(.1)
 
-    px.backward(50)
-    time.sleep(.25)
-    px.stop()
-
-    px.set_dir_servo_angle(25)
+    px.set_dir_servo_angle(-35)
     time.sleep(2)
     px.stop()
 
     px.backward(50)
-    time.sleep(.25)
-    px.stop()
-
-    px.set_dir_servo_angle(25)
-    time.sleep(2)
-    px.stop()
-
-    px.backward(50)
-    time.sleep(.25)
-    px.stop()
-
-    px.set_dir_servo_angle(0)
-    time.sleep(2)
-    px.stop()
-
-    px.forward(50)
     time.sleep(.25)
     px.stop()
     time.sleep(.1)
 
+    px.set_dir_servo_angle(-25)
+    time.sleep(2)
+    px.stop()
+
+    px.backward(50)
+    time.sleep(.55)
+    px.stop()
+    time.sleep(.1)
 
