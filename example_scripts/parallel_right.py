94fefd7556cdf6f3fca569a57be9b9ef55372129
Karina <puentek@oregonstate.edu>
2022-01-15 17:28:43 -0800

    right changes

--- example_scripts/parallel_right.py
+++ example_scripts/parallel_right.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
     px.set_dir_servo_angle(0)
     px.forward(20)
     time.sleep(3)
-    px.set_dir_servo_angle(35)
+    px.set_dir_servo_angle(30)
     time.sleep(3)
-    px.forward(10)
+    px.backward(20)
     time.sleep(3)
 
     px.stop()
