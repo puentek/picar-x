ad06eafcd987f17fc29f1e46bb97fe8623e52cef
Karina <puentek@oregonstate.edu>
2022-01-15 16:23:57 -0800

    car fb

--- example_scripts/forward_back.py
+++ example_scripts/forward_back.py
@@ -6,15 +6,16 @@ import time
 if __name__ == "__main__":
     px = Picarx()
     # move car forward (a)
-    for angle in range(0,10):
-            px.set_dir_servo_angle(angle)
-            time.sleep(0.01)
+    # for angle in range(0,10):
+    #         px.set_dir_servo_angle(angle)
+    #         time.sleep(0.01)
     # for angle in range(35,-35,-1):
     #     px.set_dir_servo_angle(angle)
     #     time.sleep(0.01)        
     # for angle in range(-35,0):
     #     px.set_dir_servo_angle(angle)
     #     time.sleep(0.01)
-    px.forward(0)
+    px.forward(20)
     time.sleep(1)
+    px.backward(20)
     
