147751e5ef6fb962dca4a886fcd2e78182455754
Karina <puentek@oregonstate.edu>
2022-01-22 15:42:33 -0800

    3.5 section

---
+++ lib/camera_drive.py
@@ -0,0 +1,40 @@
+'''The first thing to do is to isolate all the blue areas on the image. 
+To do this, we first need to turn the color space used by the image, which is RGB (Red/Green/Blue) 
+into the HSV (Hue/Saturation/Value) color space.'''
+import numpy as np 
+import cv2
+frame = cv2.imread('/home/pi/DeepPiCar/driver/data/road1_240x320.png')
+hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+#code to lift Blue out via OpenCV, and rendered mask image.
+lower_blue = np.array([60, 40, 40])
+upper_blue = np.array([150, 255, 255])
+mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+#Canny edge detection function is a powerful command that detects edges in an image
+edges = cv2.Canny(mask, 200, 400)
+
+#
+def detect_edges(frame):
+    # filter for blue lane lines
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    show_image("hsv", hsv)
+    lower_blue = np.array([60, 40, 40])
+    upper_blue = np.array([150, 255, 255])
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    show_image("blue mask", mask)
+
+    # detect edges
+    edges = cv2.Canny(mask, 200, 400)
+
+    return edges
+# detect line segment
+def detect_line_segments(cropped_edges):
+    # tuning min_threshold, minLineLength, maxLineGap is a trial and error process by hand
+    rho = 1  # distance precision in pixel, i.e. 1 pixel
+    angle = np.pi / 180  # angular precision in radian, i.e. 1 degree
+    min_threshold = 10  # minimal of votes
+    line_segments = cv2.HoughLinesP(cropped_edges, rho, angle, min_threshold, 
+                                    np.array([]), minLineLength=8, maxLineGap=4)
+
+    return line_segments
