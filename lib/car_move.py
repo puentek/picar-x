edd014e9493cbd1dab9f43ae62d667e065f1678c
Karina <puentek@oregonstate.edu>
2022-02-05 13:16:00 -0800

    week 5 changes

--- lib/car_move.py
+++ lib/car_move.py
@@ -16,28 +16,29 @@ logging.basicConfig(level=logging.DEBUG)
 if __name__ == "__main__": 
     sense_bus = Bus()
     sense_interp = Bus()
+    default_termination_bus = Bus(False)
     car = Picarx()
     sense = Sensor()
     interp = Interpreter(sense.sensitivity, sense.polarity)
     control = Controller(car)
-    consumer_producer = ConsumerProducer(consumer_producer_function,
+    consumer_producer = ConsumerProducer(interp.output,
                         input_busses= Sensor(),
                         output_busses= sense_interp,
                         delay=0,
                         termination_busses=default_termination_bus,
                         name="Unnamed consumer_producer")
-    producer =  Producer(producer_function,
+    producer =  Producer(sense.sensor_reading,
                 output_busses = sense_interp,
                 delay=0,
                 termination_busses=default_termination_bus,
                 name="Unnamed producer")
-    consumer =  Consumer(consumer_function,
+    consumer =  Consumer(control.control,
                 input_busses= Sensor(),
                 delay=0,
                 termination_busses=default_termination_bus,
                 name="Unnamed consumer")
     
-    timer = Timer(timer_busses,  # busses that should be set to true when timer triggers
+    timer = Timer(timer_busses = default_termination_bus,  # busses that should be set to true when timer triggers
                  duration=5,  # how many seconds the timer should run for (0 is forever)
                  delay=0,  # how many seconds to sleep for between checking time
                  termination_busses=default_termination_bus,
